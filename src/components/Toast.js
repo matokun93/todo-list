d503b596bc351d1480183187a25d4c89d4028571
matokun95 <matias.jca95@gmail.com>
2021-05-25 00:48:09 -0400

    Added Notes

--- src/components/Toast.js
+++ src/components/Toast.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Toast = ({ toast }) => {
 	return (
 		<div className={`${toast.visibility}`}>
-			<i class='fas fa-exclamation-triangle'></i>
+			<i className='fas fa-exclamation-triangle'></i>
 			{toast.message}
 		</div>
 	);
