5cc6b53bb611ae440ab705f0303997db5b3567fc
matokun95 <matias.jca95@gmail.com>
2021-07-15 23:23:12 -0400

    Updates

---
+++ precache-manifest.3914acdb403b4be48d78010141cea413.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eb9f4c6ae6d1a06da95b480a35f2bab",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "cef5da2ec9346017d42f",
+    "url": "/todo-list/static/css/main.1d05cf7f.chunk.css"
+  },
+  {
+    "revision": "019b5ba93fc43550e702",
+    "url": "/todo-list/static/js/2.ea74b9bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.ea74b9bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cef5da2ec9346017d42f",
+    "url": "/todo-list/static/js/main.2c1b711f.chunk.js"
+  },
+  {
+    "revision": "a557b42a7f266985029e",
+    "url": "/todo-list/static/js/runtime-main.9c8ee466.js"
+  }
+]);
